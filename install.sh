9d00e88440107e8970b8a2902dda966efe152deb
Suljov <albin.suljov@hotmail.com>
2024-08-04 16:26:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ chmod +x ReconRunner.sh
 sudo cp ReconRunner.sh /usr/local/bin/reconrunner
 
 echo '{
-    "LFI": [],
+    "lfi": [],
     "subs": ["/usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-110000.txt", "/usr/share/wordlists/seclists/Discovery/DNS/combined_subdomains.txt"],
     "dirs": ["/usr/share/wordlists/seclists/Discovery/Web-Content/quickhits.txt", "/usr/share/wordlists/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt"],
-    "SQL": [],
-    "OSinjection": []
+    "sql": [],
+    "osinjection": []
 }' > $HOME/.reconrunner/wordlists-config.json
