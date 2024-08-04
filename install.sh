10c4ab0aedb3271c6eda1f6f02a324c0045d0466
Suljov <albin.suljov@hotmail.com>
2024-08-04 16:32:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,5 +12,7 @@ echo '{
     "subs": ["/usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-110000.txt", "/usr/share/wordlists/seclists/Discovery/DNS/combined_subdomains.txt"],
     "dirs": ["/usr/share/wordlists/seclists/Discovery/Web-Content/quickhits.txt", "/usr/share/wordlists/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt"],
     "sql": [],
-    "osinjection": []
+    "osinjection": [],
+    "ssti": [],
+    "xss": []
 }' > $HOME/.reconrunner/wordlists-config.json
