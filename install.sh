1e2963449e808b006e833540dda591b5d537811c
suljov <albin.suljov@hotmail.com>
2024-12-01 16:52:06 +0100

    update install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cp wordlists-config.json ~/.reconrunner/wordlists-config.json
-cp ReconRunner.py /usr/local/bin/reconrunner
+sudo cp ReconRunner.py /usr/local/bin/reconrunner
+sudo chmod +x /usr/local/bin/reconrunner
